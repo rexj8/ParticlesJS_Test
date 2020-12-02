a112100e8d435fdf6d647d8679032e24c336fadf
rexj8 <rachit.gla_cs18@gla.ac.in>
2020-12-02 13:11:37 +0530

    testing

--- test3_bear-snowfall/partical.js/app.js
+++ test3_bear-snowfall/partical.js/app.js
@@ -23,7 +23,7 @@ particlesJS('snow-fall',
         }
       },
       "color": {
-        "value": ["#F2FFFD","#ffffff","95FFEF"]
+        "value": ["#F2FFFD","#ffffff","95FFEF","3DCFB9"]
       },
       "shape": {
         "type": "circle",
