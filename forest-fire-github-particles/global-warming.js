8753e2ed9fc934bbcfe17efbc31e23ef8508aa1d
rexj8 <rachit.gla_cs18@gla.ac.in>
2020-12-06 09:46:56 +0530

    testing

--- forest-fire-github-particles/global-warming.js
+++ forest-fire-github-particles/global-warming.js
@@ -18,7 +18,7 @@ function myFunction() {
 
             x.style.display = "flex";
             x.style.position = "fixed";
-            x.style.background = "linear-gradient(to right,rgb(235,236,238),rgb(239,241,240),rgb(231,235,234),rgb(230,224,225))";
+            x.style.background = "linear-gradient(to right,rgb(225, 226, 210),rgb(224, 223, 203),rgb(219, 216, 199),rgb(214, 211, 192))";
             x.style.alignContent = "center";
             x.style.justifyContent = "space-around";
             x.style.width = "100%";
@@ -34,6 +34,7 @@ function myFunction() {
 // ----------------------------------------------------------------------------------------------------------------------------
 
 
+
 // ---------------------------------Burger sign------------------------------------------------------------------------------
 const navSlide = () => {
     
@@ -43,7 +44,7 @@ const navSlide = () => {
 
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
-        nav.style='background:linear-gradient(to bottom,rgb(227,231,232),rgb(160,173,179))';
+        nav.style='background:linear-gradient(to right,rgb(225, 226, 210),rgb(224, 223, 203),rgb(219, 216, 199),rgb(214, 211, 192))';
 
         navLinks.forEach((link, index) => {
             if (link.style.animation) {
@@ -63,3 +64,5 @@ const navSlide = () => {
 
 navSlide();
 // ----------------------------------------------------------------------------------------------------------------------------
+
+
